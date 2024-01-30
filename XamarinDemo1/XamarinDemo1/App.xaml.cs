4903c7bda41e75d49b3ed879ff5c8f2975c4b79a
Adria Navarro <adrian@vmware.com>
2024-01-30 15:30:04 -0600

    first commit

---
+++ XamarinDemo1/XamarinDemo1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinDemo1.Helpers;
+
+namespace XamarinDemo1
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            Console.WriteLine($"Fruit is {AppSettings.fruit}");
+            Console.WriteLine($"foo is {Secrets.foo}");
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
---
+++ XamarinDemo1/XamarinDemo1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDemo1
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Fruit { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Fruit = Helpers.AppSettings.fruit;
+            BindingContext = this;
+        }
+    }
+}
+
